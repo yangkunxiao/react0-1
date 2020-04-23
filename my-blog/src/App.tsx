4018ec427e065d609cd24ed7a486e604e3d1e4fd
yangkunxiao <15738518697@163.com>
2020-04-23 20:52:40 +0800

    安装axios

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
-import Menu from './components/menu'
-
+import Menu from './components/menu';
+import {fetchData} from './api/axios';
+import md5 from 'md5';
 const list = [
     {
         name: 'string',
 	    url: 'string'
     }
 ]
+
+const data = {
+    url:'/employee/login',
+    body:{
+        "loginName": 1234567890,
+        "loginPass": md5('111111'),
+        "appId": "2",
+        "loginType": "1"
+    }
+}
+
+fetchData({
+    url:'/employee/login',
+    body:{
+        "loginName": 1234567890,
+        "loginPass": md5('111111'),
+        "appId": "2",
+        "loginType": "1"
+    },
+    callack:(res) => {
+        console.log(res)
+    }
+})
 
 function App() {
   return (
